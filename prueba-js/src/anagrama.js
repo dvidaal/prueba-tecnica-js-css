35e8c31795670abfee1bfea92657c3dc03e68372
Diana Vidal <dvidaal@gmail.com>
2025-01-18 12:15:08 +0100

    Add solution for anagrama.js

--- prueba-js/src/anagrama.js
+++ prueba-js/src/anagrama.js
@@ -1,5 +1,26 @@
+const normalizeWords = (word1, word2) => {
+  if (!word1 && word1) return "";
+
+  const word1Normalized = word1
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/,/g, "")
+    .replace(/[\u0300-\u036f]/g, "")
+    .split("")
+    .sort()
+    .join("");
+  const word2Normalized = word2
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/,/g, "")
+    .replace(/[\u0300-\u036f]/g, "")
+    .split("")
+    .sort()
+    .join("");
+
+  return word1Normalized === word2Normalized;
+};
+
 export default (word1, word2) => {
-    // Do your thing here!
-    return false;
-  };
-  
+  return normalizeWords(word1, word2);
+};
